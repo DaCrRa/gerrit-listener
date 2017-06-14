27c66626246e2faf1b8e401a27aa504f8f3469c2
Dani C R <danielcr86@gmail.com>
2017-06-14 17:22:22 +0100

    ReviewerAddedFilter: filter on reviewer username
    The filter receives a list of user names.
    
    Events will be filtered if they are reviewer added
    and the reviewer is in the list. Empty list means
    any reviewer will be filtered.

--- filters/event_type_filters.py
+++ filters/event_type_filters.py
@@ -4,5 +4,14 @@ from pygerrit.events import ReviewerAddedEvent
 
 class ReviewerAddedFilter(GerritEventFilter):
 
+   def __init__(self, filtered_reviewers=[]):
+      self.filtered_reviewers=filtered_reviewers
+
    def filter_event(self, event):
-      return isinstance(event, ReviewerAddedEvent)
+      if isinstance(event, ReviewerAddedEvent):
+         if len(self.filtered_reviewers) > 0:
+            return event.reviewer.username in self.filtered_reviewers
+         else:
+            return True
+      else:
+         return False

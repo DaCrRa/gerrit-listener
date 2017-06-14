2f75d35c78f34364f185152769bea11b1e414722
Dani C R <danielcr86@gmail.com>
2017-06-14 17:07:47 +0100

    Rename filters/ReviewerAddedFilter.py -> filters/event_type_filters.py
    So we can add to this file different filter classes,
    all them related with event type

--- listen_and_log.py
+++ listen_and_log.py
@@ -1,4 +1,4 @@
-from filters.ReviewerAddedFilter import ReviewerAddedFilter
+from filters.event_type_filters import ReviewerAddedFilter
 from actions.PrintEvent import PrintEvent
 from action_executors.SimpleActionExecutor import SimpleActionExecutor
 
